6edb01dbcf2086f3e57ae0682d690ad0ba67575e
Visvajeetraj <visvajeetraj@gmail.com>
2026-03-26 13:43:45 +0530

    Fix trailer Video player configuration error 153

--- script.js
+++ script.js
@@ -1026,7 +1026,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                         <iframe 
                             width="100%" 
                             height="100%" 
-                            src="https://www.youtube.com/embed/${trailerId}" 
+                            src="https://www.youtube-nocookie.com/embed/${trailerId}?rel=0&modestbranding=1" 
                             title="YouTube video player"
                             frameborder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
