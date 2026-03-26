382ccb0137f9d5f4dddece9f06d351354bba1584
Visvajeetraj <visvajeetraj@gmail.com>
2026-03-26 13:47:36 +0530

    Fix youtube embed Error 153 using strict referrer policy

--- script.js
+++ script.js
@@ -1026,10 +1026,11 @@ document.addEventListener('DOMContentLoaded', async () => {
                         <iframe 
                             width="100%" 
                             height="100%" 
-                            src="https://www.youtube-nocookie.com/embed/${trailerId}?rel=0&modestbranding=1" 
+                            src="https://www.youtube.com/embed/${trailerId}" 
                             title="YouTube video player"
                             frameborder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            referrerpolicy="strict-origin-when-cross-origin"
                             allowfullscreen>
                         </iframe>
                     </div>
